50a5c9930913434cf7e157564b38e1da3c6fbfbc
Stellar Ashes <xmc3ify@gmail.com>
2016-04-01 19:39:13 -0400

    If length is greater than 60 for TTS, try to split off into phrases for chrome to speak

--- angular/services/text-to-speech-service.js
+++ angular/services/text-to-speech-service.js
@@ -5,6 +5,8 @@
 		.factory('ttsService', TextToSpeechService);
 	
 	TextToSpeechService.$inject = ['configService', '$q'];
+
+	var delimiters = /[、。…？！]/;
 	
 	function TextToSpeechService(configService, $q) {
 		return {
@@ -16,8 +18,22 @@
 		function speak(phrase) {
 			configService.get(['lang', 'rate', 'voiceName']).then(function (options) {
 				phrase = phrase.replace(/[…]+/g, '.');
-				chrome.tts.speak(phrase, options);
+				speakRaw(phrase, options);
 			});
+		}
+
+		function speakRaw(phrase, options) {
+			if (phrase.length > 60) {
+				var utterances = phrase.split(delimiters);
+				chrome.tts.speak(utterances[0], options);
+				options.enqueue = true;
+				for (var i = 1; i < utterances.length; i++) {
+					var obj = utterances[i];
+					chrome.tts.speak(obj, options);
+				}
+			} else {
+				chrome.tts.speak(phrase, options);
+			}
 		}
 
 		function stop() {
